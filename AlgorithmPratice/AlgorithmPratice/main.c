de16654a0fd57db44bb7d336631ad5fd00b81833
wangqizheng <wangqizheng@58ganji.com>
2019-04-01 11:49:00 +0800

    提交第一个关于的栈的引用-递归

---
+++ AlgorithmPratice/AlgorithmPratice/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  AlgorithmPratice
+//
+//  Created by 王启正 on 2019/3/29.
+//  Copyright © 2019 隔壁老王. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Recursive.h"
+
+
+void recursive(void);
+
+int main(int argc, const char * argv[]) {
+    
+    
+    recursive();
+    
+    return 0;
+}
+void recursive(){
+    
+//    normalFbi();
+    callRecursiveFbi();
+    
+}
