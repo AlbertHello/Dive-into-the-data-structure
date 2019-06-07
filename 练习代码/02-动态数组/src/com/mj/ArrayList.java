71d7553ce12af7844a95ba1a5873b7b01795dd8a
AlbertHello <815878037@qq.com>
2019-06-07 20:08:07 +0800

    动态数组练习

---
+++ 练习代码/02-动态数组/src/com/mj/ArrayList.java
@@ -0,0 +1,203 @@
+package com.mj;
+
+@SuppressWarnings("unchecked")//警告忽略
+public class ArrayList<E> {//<E>代表泛型，可以放任何对象类型的元素
+	/**
+	 * 元素的数量
+	 */
+	private int size;
+	/**
+	 * 所有的元素
+	 */
+	private E[] elements;
+	
+	private static final int DEFAULT_CAPACITY = 10;
+	private static final int ELEMENT_NOT_FOUND = -1;
+	
+	public ArrayList(int capaticy) {
+		capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
+		elements = (E[]) new Object[capaticy];//Object是Java中任何类的基类。前面在用(E[])强转成需要的类型
+	}
+	
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
+	}
+	
+	/**
+	 * 清除所有元素
+	 */
+	public void clear() {
+//		清空数组时不能简单的领size=0，因为数组中存储着各个对象的地址，地址对应的对象还占着内存，需要销毁这些对象，
+		for (int i = 0; i < size; i++) {
+			elements[i] = null;
+		}
+		size = 0;
+	}
+
+	/**
+	 * 元素的数量
+	 * @return
+	 */
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * 是否为空
+	 * @return
+	 */
+	public boolean isEmpty() {
+		 return size == 0;
+	}
+
+	/**
+	 * 是否包含某个元素
+	 * @param element
+	 * @return
+	 */
+	public boolean contains(E element) {
+		return indexOf(element) != ELEMENT_NOT_FOUND;
+	}
+
+	/**
+	 * 添加元素到尾部
+	 * @param element
+	 */
+	public void add(E element) {
+		add(size, element);//追加元素那直接index就直接等size即可，然后size再加一
+	}
+
+	/**
+	 * 获取index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E get(int index) {
+		rangeCheck(index);
+		return elements[index];
+	}
+
+	/**
+	 * 设置index位置的元素
+	 * @param index
+	 * @param element
+	 * @return 原来的元素ֵ
+	 */
+	public E set(int index, E element) {
+		rangeCheck(index);
+		
+		E old = elements[index];
+		elements[index] = element;
+		return old;
+	}
+
+	/**
+	 * 在index位置插入一个元素
+	 * @param index
+	 * @param element
+	 */
+	public void add(int index, E element) {
+		rangeCheckForAdd(index);
+		
+		ensureCapacity(size + 1);
+		
+		for (int i = size - 1; i >= index; i--) {
+			elements[i + 1] = elements[i];
+		}
+		elements[index] = element;
+		size++;
+	}
+
+	/**
+	 * 删除index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E remove(int index) {
+		rangeCheck(index);
+		
+		E old = elements[index];
+		for (int i = index + 1; i <= size - 1; i++) {
+//			删除某个元素，要把这个元素后的所有元素往前移动一个单位
+			elements[i - 1] = elements[i];//前面的元素被后面的元素覆盖
+		}
+		elements[--size] = null;
+		return old;
+	}
+
+	/**
+	 * 查看元素的索引
+	 * @param element
+	 * @return
+	 */
+	public int indexOf(E element) {
+		if (element == null) {
+			for (int i = 0; i < size; i++) {
+				if (elements[i] == null) return i;//这个只会返回第一个元素是null的index
+			}
+		} else {
+			for (int i = 0; i < size; i++) {
+				//equals函数默认比较两个对象的地址是否相同。自定义对象可以重写这个方法，自定义怎么样两个对象才算是相等
+				if (element.equals(elements[i])) return i;
+			}
+		}
+		return ELEMENT_NOT_FOUND;
+	}
+	
+	/**
+	 * 保证要有capacity的容量
+	 * @param capacity
+	 */
+	private void ensureCapacity(int capacity) {
+		int oldCapacity = elements.length;
+		if (oldCapacity >= capacity) return;
+		 
+		// 新容量为旧容量的1.5倍
+		int newCapacity = oldCapacity + (oldCapacity >> 1);//右移一位表示除2，
+		E[] newElements = (E[]) new Object[newCapacity];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];//把旧数组里的元素挨个赋值给新数组
+		}
+		elements = newElements;//elements重新指向新数组，则原先指向的旧数组会被自动释放。
+		
+		System.out.println(oldCapacity + "扩容为" + newCapacity);
+	}
+//	越界抛出异常
+	private void outOfBounds(int index) {
+		throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+	}
+//	越界检查
+	private void rangeCheck(int index) {
+		if (index < 0 || index >= size) {
+			outOfBounds(index);
+		}
+	}
+	
+//	对于添加元素的越界检查
+	private void rangeCheckForAdd(int index) {
+		if (index < 0 || index > size) {
+			outOfBounds(index);
+		}
+	}
+	
+	@Override
+	public String toString() {
+//		Java中打印某个类时实际就是调用了toString方法，这里重写这个方法按照自己的方式打印
+		// size=3, [99, 88, 77]
+		StringBuilder string = new StringBuilder();
+		string.append("size=").append(size).append(", [");
+		for (int i = 0; i < size; i++) {
+			if (i != 0) {
+				string.append(", ");
+			}
+			
+			string.append(elements[i]);
+			
+//			if (i != size - 1) {
+//				string.append(", ");
+//			}
+		}
+		string.append("]");
+		return string.toString();
+	}
+}
