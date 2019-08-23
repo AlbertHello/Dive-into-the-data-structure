50de2197a81a0db8bd5087af5c1006def2a95b3f
AlbertHello <815878037@qq.com>
2019-08-23 10:35:46 +0800

    调整目录

--- C语言实现/链表/链表/ViewController.m
+++ C语言实现/链表/链表/ViewController.m
@@ -25,7 +25,12 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor=[UIColor redColor];
     
+    //单链表
+    [self testSingleLink];
     
+}
+
+-(void)testSingleLink{
     Link head;//头指针
     Status ret = init_link(&head);
     if (ret!=Success) {
@@ -45,26 +50,24 @@
     int data;
     delete_element(2, &data, head);
     printLink(head);
-
+    
     //根据index查找ele
     Link node;
     node_of_index(4, head, &node);
     printf("查找的第四个元素是：%d\n",node->data);
-
+    
     //修改
     update_ele(5, 90, head);
     printLink(head);
-
+    
     //查找
     int index=locate_ele_with_element(30, head);
     printf("30是在第%d个节点\n",index+1);
     int index1=locate_ele_with_element(55, head);
     printf("55是在第%d个节点\n",index1);
-
-
+    
     clearLink(head);
 }
-
 
 
 
