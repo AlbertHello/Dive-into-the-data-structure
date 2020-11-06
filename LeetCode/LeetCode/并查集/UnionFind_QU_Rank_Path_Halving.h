e96a236bffbf9a85cfe72b28087bcf68c823cd25
wangqizheng <wangqizheng@58.com>
2020-11-06 12:44:29 +0800

    路径减半

---
+++ LeetCode/LeetCode/并查集/UnionFind_QU_Rank_Path_Halving.h
@@ -0,0 +1,16 @@
+//
+//  UnionFind_QU_Rank_Path_Halving.h
+//  LeetCode
+//
+//  Created by 58 on 2020/11/6.
+//
+
+#import "UnionFind_QU_Rank.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnionFind_QU_Rank_Path_Halving : UnionFind_QU_Rank
+
+@end
+
+NS_ASSUME_NONNULL_END
