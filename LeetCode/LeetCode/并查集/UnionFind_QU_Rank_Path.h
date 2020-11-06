e777b37ce514546e96d4a68a8b0abe096ca5773a
wangqizheng <wangqizheng@58.com>
2020-11-06 10:32:40 +0800

    union find quick union

---
+++ LeetCode/LeetCode/并查集/UnionFind_QU_Rank_Path.h
@@ -0,0 +1,16 @@
+//
+//  UnionFind_QU_Rank_Path.h
+//  LeetCode
+//
+//  Created by 58 on 2020/11/5.
+//
+
+#import "UnionFind_QU_Rank.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnionFind_QU_Rank_Path : UnionFind_QU_Rank
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeetCode/LeetCode/并查集/UnionFind_QU_Rank_Path.m
@@ -0,0 +1,13 @@
+//
+//  UnionFind_QU_Rank_Path.m
+//  LeetCode
+//
+//  Created by 58 on 2020/11/5.
+//
+
+#import "UnionFind_QU_Rank_Path.h"
+
+// 路径压缩
+@implementation UnionFind_QU_Rank_Path
+
+@end
