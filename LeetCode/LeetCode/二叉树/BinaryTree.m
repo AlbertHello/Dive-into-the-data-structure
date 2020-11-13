30cbaf4fe05af741fa9880fd4a5854c6bc1a4df3
wangqizheng <wangqizheng@58.com>
2020-11-13 18:17:42 +0800

    116 填充二叉树节点的右侧指针 226 反转二叉树

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -6,8 +6,125 @@
 //
 
 #import "BinaryTree.h"
+@interface BTNode : NSObject
+
+@property(assign, nonatomic)NSInteger data;
+@property(strong, nonatomic,nullable)BTNode *parent;
+@property(strong, nonatomic,nullable)BTNode *left;
+@property(strong, nonatomic,nullable)BTNode *right;
+@end
+
+@implementation BTNode
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.data=0;
+        self.parent=nil;
+        self.left=nil;
+        self.right=nil;
+    }
+    return self;
+}
+@end
+
+
+@interface BTNode1 : NSObject
+
+@property(assign, nonatomic)NSInteger data;
+@property(strong, nonatomic,nullable)BTNode1 *parent;
+@property(strong, nonatomic,nullable)BTNode1 *left;
+@property(strong, nonatomic,nullable)BTNode1 *right;
+@property(strong, nonatomic,nullable)BTNode1 *next;
+@end
+
+@implementation BTNode1
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.data=0;
+        self.parent=nil;
+        self.left=nil;
+        self.right=nil;
+    }
+    return self;
+}
+@end
+
 
 @implementation BinaryTree
+
+//************************* 226 反转二叉树 *************************
+
+//1、递归处理。三种遍历方式都可以
+-(BTNode *)invertTree1:(BTNode *)root{
+    if (root == NULL) return root;
+    BTNode *node=root.left;
+    root.left=root.right;
+    root.right=node;
+    [self invertTree1:root.left];
+    [self invertTree1:root.right];
+    return root;
+}
+-(BTNode *)invertTree2:(BTNode *)root{
+    if (root == NULL) return root;
+    [self invertTree2:root.left];
+    BTNode *node=root.left;
+    root.left=root.right;
+    root.right=node;
+    [self invertTree2:root.left];
+    return root;
+}
+-(BTNode *)invertTree3:(BTNode *)root{
+    if (root == NULL) return root;
+    [self invertTree3:root.left];
+    [self invertTree3:root.right];
+    BTNode *node=root.left;
+    root.left=root.right;
+    root.right=node;
+    return root;
+}
+//迭代-使用队列这里使用数组代替,其实就是二叉树的层序遍历
+-(BTNode *)invertTree4:(BTNode *)root{
+    if (root == NULL) return root;
+    NSMutableArray *queue=[NSMutableArray array];
+    [queue addObject:root];
+    
+    while (queue.count != 0) {
+        BTNode *node = queue.firstObject;
+        BTNode *tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+        [queue removeObjectAtIndex:0];
+        
+        if (node.left != NULL) {
+            [queue addObject:node.left];
+        }
+        if (node.right != NULL) {
+            [queue addObject:node.right];
+        }
+    }
+    return root;
+}
+
+//************************* 116 填充二叉树节点的右侧指针 *************************
+
+-(BTNode1 *)connect:(BTNode1 *)root{
+    if (root == nil || root.left == nil) return root;
+    [self connectTwoNode:root.left node:root.right];
+    return root;
+}
+-(void)connectTwoNode:(BTNode1 *)node1 node:(BTNode1 *)node2{
+    /**** 前序遍历位置 ****/
+    // 将传入的两个节点连接
+    node1.next = node2;
+    // 连接相同父节点的两个子节点
+    [self connectTwoNode:node1.left node:node1.right];
+    [self connectTwoNode:node2.left node:node2.right];
+    // 连接跨越父节点的两个子节点
+    [self connectTwoNode:node1.right node:node2.left];
+}
 
 
 
