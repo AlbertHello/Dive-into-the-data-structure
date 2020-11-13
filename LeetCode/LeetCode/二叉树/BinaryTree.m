8d068ccc4da3371dfbb4c4bef91241607651ecc8
wangqizheng <wangqizheng@58.com>
2020-11-13 20:31:42 +0800

    最大二叉树

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -156,7 +156,64 @@
     }
     p.right = right;
 }
+//************************* 654 最大二叉树 *************************
+/**
+ 654. 最大二叉树
+ 给定一个不含重复元素的整数数组。一个以此数组构建的最大二叉树定义如下：
+ 二叉树的根是数组中的最大元素。
+ 左子树是通过数组中最大值左边部分构造出的最大二叉树。
+ 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+ 通过给定的数组构建最大二叉树，并且输出这个树的根节点。
+ 示例 ：
+ 输入：[3,2,1,6,0,5]
+ 输出：返回下面这棵树的根节点：
 
+       6
+     /   \
+    3     5
+     \    /
+      2  0
+        \
+         1
+ */
 
+/**
+ 肯定要遍历数组把找到最大值 maxVal，把根节点 root 做出来，然后对 maxVal 左边的数组和右边的数组进行递归调用，作为 root 的左右子树。
+  伪代码如下所示：
+ TreeNode constructMaximumBinaryTree([3,2,1,6,0,5]) {
+     // 找到数组中的最大值
+     TreeNode root = new TreeNode(6);
+     // 递归调用构造左右子树
+     root.left = constructMaximumBinaryTree([3,2,1]);
+     root.right = constructMaximumBinaryTree([0,5]);
+     return root;
+ }
+ */
+/* 主函数 */
+-(BTNode *)constructMaximumBinaryTree:(NSArray *)array {
+    
+    return [self build:array low:0 height:(int)array.count-1];
+}
 
+/* 将 nums[lo..hi] 构造成符合条件的树，返回根节点 */
+-(BTNode *)build:(NSArray *)array low:(int)lo height:(int)hi{
+    // base case
+    if (lo > hi) return nil;
+
+    // 找到数组中的最大值和对应的索引
+    int index = -1, maxVal = INT_MIN;
+    for (int i = lo; i <= hi; i++) {
+        if (maxVal < [array[i] intValue]) {
+            index = i;
+            maxVal = [array[i] intValue];
+        }
+    }
+    BTNode *root=[[BTNode alloc]init];
+    root.data=maxVal;
+    // 递归调用构造左右子树
+    root.left = [self build:array low:lo height:index -1];
+    root.right = [self build:array low:index+1 height:hi];
+
+    return root;
+}
 @end
