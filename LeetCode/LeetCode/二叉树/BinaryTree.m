7f8e96cfefad0f349172186103a385180147479d
wangqizheng <wangqizheng@58.com>
2020-11-17 12:19:32 +0800

    112. 路径总和

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -8,7 +8,7 @@
 #import "BinaryTree.h"
 @interface BTNode : NSObject
 
-@property(assign, nonatomic)NSInteger data;
+@property(assign, nonatomic)int data;
 @property(strong, nonatomic,nullable)BTNode *parent;
 @property(strong, nonatomic,nullable)BTNode *left;
 @property(strong, nonatomic,nullable)BTNode *right;
@@ -381,8 +381,8 @@ static int result = 0;
 }
 /**
  时间复杂度：O(n)，其中n 是二叉树的节点个数。对每个节点访问一次。
- 空间复杂度：O(n)，其中n 是二叉树的节点个数。空间复杂度主要取决于递归调用的栈空间，
- 递归栈的深度等于二叉树的高度，最坏情况下，二叉树的高度等于节点个数，空间复杂度为O(n)。
+ 空间复杂度：O(H)，其中H 是树的高度。空间复杂度主要取决于递归时栈空间的开销，最坏情况下，树呈现链状，
+ 空间复杂度为O(N)。平均情况下树的高度与节点数的对数正相关，空间复杂度为O(logN)。
  */
 -(int)sumNumbers:(BTNode *)root {
     path=[NSMutableArray array];
@@ -391,5 +391,113 @@ static int result = 0;
     [self traversal:root];
     return result;
 }
+//************************* 112. 路径总和 *************************
+/**
+ 112. 路径总和
+ 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ 说明: 叶子节点是指没有子节点的节点。
+ 示例:
+ 给定如下二叉树，以及目标和 sum = 22，
+               5
+              / \
+             4   8
+            /   / \
+           11  13  4
+          /  \      \
+         7    2      1
+ 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ https://leetcode-cn.com/problems/path-sum/
+ */
+/**
+ 递归
+1 可以使用深度优先遍历的方式，确定递归函数的参数和返回类型
+ 参数：需要二叉树的根节点，还需要一个计数器，这个计数器用来计算二叉树的一条边之和是否正好是目标和，计数器为int型。来看返回值，递归函数什么时候需要返回值？什么时候不需要返回值？
+ 在《二叉树：我的左下角的值是多少？》中得出结论：如果需要搜索整颗二叉树，那么递归函数就不要返回值，如果要搜索其中一条符合条件的路径，递归函数就需要返回值，因为遇到符合条件的路径了就要及时返回。
+ 在《二叉树：我的左下角的值是多少？》因为要遍历树的所有路径，找出深度最深的叶子节点，所以递归函数不要返回值。而本题我们要找一条符合条件的路径，所以递归函数需要返回值，及时返回，
+2 确定终止条件
+ 首先计数器如何统计这一条路径的和呢？
+ 不要去累加然后判断是否等于目标和，那么代码比较麻烦，可以用递减，让计数器count初始为目标和，然后每次减去遍历路径节点上的数值。如果最后count == 0，同时到了叶子节点的话，说明找到了目标和。
+ 如果遍历到了叶子节点，count不为0，就是没找到。
+ 3 确定单层递归的逻辑
+ 因为终止条件是判断叶子节点，所以递归的过程中就不要让空节点进入递归了。
+ 递归函数是有返回值的，如果递归函数返回true，说明找到了合适的路径，应该立刻返回。
+ 
+ 时间复杂度：O(N)，其中N 是树的节点数。对每个节点访问一次。
+ 空间复杂度：O(H)，其中H 是树的高度。空间复杂度主要取决于递归时栈空间的开销，最坏情况下，树呈现链状，
+ 空间复杂度为O(N)。平均情况下树的高度与节点数的对数正相关，空间复杂度为O(logN)。
+ 
+ */
+
+//回溯隐藏在traversal(cur->left, count - cur->left->val)这里， 因为把count - cur->left->val 直接作为参数传进去，函数结束，count的数值没有改变。
+//为了把回溯的过程体现出来，可以改为如下代码：
+-(BOOL)traversal_112:(BTNode*)cur count:(int)count {
+    if (!cur.left && !cur.right && count == 0) return true; // 遇到叶子节点，并且计数为0
+    if (!cur.left && !cur.right) return false; // 遇到叶子节点直接返回
+    if (cur.left) { // 左
+        count -= cur.left.data; // 递归，处理节点;
+        if ([self traversal_112:cur.left count:count]) return true;
+        count += cur.left.data; // 回溯，撤销处理结果
+    }
+    if (cur.right) { // 右
+        count -= cur.right.data; // 递归，处理节点;
+        if ([self traversal_112:cur.right count:count]) return true;
+        count += cur.right.data; // 回溯，撤销处理结果
+    }
+    return false;
+}
+-(BOOL)hasPathSum1:(BTNode*)root sum:(int)sum {
+    if (root == nil) return false;
+    return [self traversal_112:root count:sum-root.data];
+}
+//以上代码精简之后如下
+-(BOOL)hasPathSum2:(BTNode*)root sum:(int)sum {
+    if (root == NULL) return false;
+    if (!root.left && !root.right && sum == root.data) return true;
+    return [self hasPathSum2:root.left sum:sum-root.data] || [self hasPathSum2:root.right sum:sum-root.data];
+}
+
+/**
+ 解法2 前序遍历迭代
+ */
+-(BOOL)hasPathSum3:(BTNode*)root sum:(int)sum {
+    if (root == NULL) return false;
+    NSMutableArray *stack=[NSMutableArray array];
+    // 此时栈里要放的是<节点指针, 还要记录从头结点到该节点的路径数值总和>
+    NSDictionary *dict=@{
+        @"node":root,
+        @"count":@(root.data)
+    };
+    [stack addObject:dict];
+    while (stack.count != 0) {
+        //访问节点
+        NSDictionary *dict =(NSDictionary *)stack.lastObject;
+        [stack removeLastObject];
+        // 如果该节点是叶子节点了，同时该节点的路径数值等于sum，那么就返回true
+        BTNode *node=dict[@"node"];
+        int count=[dict[@"count"] intValue];
+        if (!node.left && !node.right && sum == count) return true;
+        // 右节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+        if (node.right) {
+            NSDictionary *right=@{
+                @"node":node.right,
+                @"count":@(count+node.right.data)
+            };
+            [stack addObject:right];
+        }
+        // 左节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+        if (node.left) {
+            NSDictionary *left=@{
+                @"node":node.left,
+                @"count":@(count+node.left.data)
+            };
+            [stack addObject:left];
+        }
+    }
+    return false;
+}
+
+
+
+
 
 @end
