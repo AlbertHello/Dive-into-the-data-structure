fa903f18898368edabff76af99c08125d63e184f
wangqizheng <wangqizheng@58.com>
2020-12-02 15:07:11 +0800

    每日温度

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -368,6 +368,33 @@ int *parentIndexes(int *nums, int length) {
     }
     return pis;
 }
+/**
+ 则有如下题目：
+ 739. 每日温度
+ https://leetcode-cn.com/problems/daily-temperatures/
+ 请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ 
+ 这个题其实就只是找某个数的右边的第一个比它大的数
+ 
+ */
+int* dailyTemperatures(int* T, int length) {
+    if (T == NULL || length == 0) return NULL;
+    int *result = (int *)malloc(sizeof(int)*length);
+    Stack *stack=[[Stack alloc]init]; // 栈中存储的是数组索引
+    for (int i = 0; i < length; i++) {
+        // 这里应该要写大于，不要写大于等于
+        while (!stack.isEmpty && T[i] > T[stack.peek]) {
+            // 如果将要压栈的这个元素比栈顶元素大，那么T[i]就是比T[stack.peek]的右边第一个大的数
+            result[stack.peek] = i - stack.peek; // i - stack.peek 就是两个数的索引之差
+            [stack pop]; // 删除这个栈顶元素，再循环，T[i]再和下一个栈顶元素比较
+        }
+        [stack push:i]; // 栈为空或T[i]小于栈顶元素。直接入栈。
+    }
+    return result;
+}
+
 
 
 
