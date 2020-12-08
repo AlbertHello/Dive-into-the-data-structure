1a95605a714719bfad59c0ca71aa1f7c4a6fe9cd
wangqizheng <wangqizheng@58.com>
2020-12-08 21:18:07 +0800

    50. Pow(x, n)

---
+++ LeetCode/LeetCode/算法/_50_My_Pow.h
@@ -0,0 +1,16 @@
+//
+//  _50_My_Pow.h
+//  LeetCode
+//
+//  Created by 58 on 2020/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _50_My_Pow : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
