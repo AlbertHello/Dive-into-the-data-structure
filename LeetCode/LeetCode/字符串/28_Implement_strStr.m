121f7d45d5000b219414838b163f0cb87b86c168
wangqizheng <wangqizheng@58.com>
2020-12-14 12:59:19 +0800

    实现 strStr()函数 双指针

--- LeetCode/LeetCode/字符串/28_Implement_strStr.m
+++ LeetCode/LeetCode/字符串/28_Implement_strStr.m
@@ -12,7 +12,7 @@
 
 /**
  28. 实现 strStr()函数
- 
+ 链接：https://leetcode-cn.com/problems/implement-strstr
  给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
 
  示例 1:
@@ -35,10 +35,6 @@
 
  0 <= haystack.length, needle.length <= 5 * 104
  haystack and needle consist of only lower-case English characters.
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/implement-strstr
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  
  当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
 
@@ -78,7 +74,50 @@ int strStr(char * haystack, char * needle){
     }
     return -1;
 }
+/**
+ 解法2 双指针法
+ 上一个方法的缺陷是会将 haystack 所有长度为 L 的子串都与 needle 字符串比较，实际上是不需要这么做的。
 
+ 首先，只有子串的第一个字符跟 needle 字符串第一个字符相同的时候才需要比较，
+ 然后，可以一个字符一个字符比较，一旦不匹配了就立刻终止。
+ 比较到最后一位时发现不匹配，这时候开始回溯。需要注意的是，pn 指针是移动到 pn = pn - curr_len + 1 的位置，而 不是 pn = pn - curr_len 的位置。
+ 
+ 算法：
+ 移动 pn 指针，直到 pn 所指向位置的字符与 needle 字符串第一个字符相等。
+ 通过 pn，pL，curr_len 计算匹配长度。
+ 如果完全匹配（即 curr_len == L），返回匹配子串的起始坐标（即 pn - L）。
+ 如果不完全匹配，回溯。使 pn = pn - curr_len + 1， pL = 0， curr_len = 0。
+ 
+ 时间复杂度：最坏时间复杂度为O((N−L)L)，最优时间复杂度为O(N)。
+ 空间复杂度：O(1)。
+ 
+ */
+int strStr2(char *haystack, char *needle) {
+    
+    int n = (int)strlen(haystack);
+    int L = (int)strlen(needle);
+    if (L == 0) return 0;
+    if (L > n) return -1;
+    
+    int pn = 0;
+    while (pn < n - L + 1) { // needl最坏情况下就是haystack的最后L个字符正好匹配
+        // 在haystack找到第一个匹配needle的位置
+        while (pn < n - L + 1 && haystack[pn] != needle[0]) ++pn;
+        
+        // 匹配
+        int currLen = 0, pL = 0;
+        while (pL < L && pn < n && haystack[pn] == needle[pL]){
+            ++pn;
+            ++pL;
+            ++currLen;
+        }
+        // 找到了
+        if (currLen == L) return pn - L;
+        // 回退到第一个相同字符处的下一个
+        pn = pn - currLen + 1;
+    }
+    return -1;
+}
 
 
 
