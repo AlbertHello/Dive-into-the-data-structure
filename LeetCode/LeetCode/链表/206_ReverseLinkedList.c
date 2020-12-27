0f6eeb6c76fb159cab58984439c732d39f05a55b
wangqizheng <wangqizheng@58.com>
2020-12-27 11:12:47 +0800

    递归反转链表

--- LeetCode/LeetCode/链表/206_ReverseLinkedList.c
+++ LeetCode/LeetCode/链表/206_ReverseLinkedList.c
@@ -24,7 +24,8 @@ SingleNode_1 *reverseList_C1(SingleNode_1* head){
     if (head == NULL || head->next == NULL){
         return head; // 当开始返回时，说明传进来的head=1
     }
-    //这个意思是 把2->3->4->5->NULL反转成5->4->3->2->null
+    // 这句代码意思是 把2->3->4->5->NULL反转成5->4->3->2->null
+    // 不要用脑袋去想压栈的详细过程，因为脑袋压不了几次栈
     SingleNode_1 *new_head=reverseList_C1(head->next);
     //此时1->next=2, 2->null,
     //这句代码就是让2指向1
@@ -98,3 +99,43 @@ SingleNode_1 * reverseKGroup(SingleNode_1 * head, int k) {
 }
 
 
+/**
+ 将链表的前 n 个节点反转（n <= 链表长度）
+ 反转以 head 为起点的 n 个节点，返回新的头结点
+ 
+ 1 base case 变为n == 1，反转一个元素，就是它本身，同时要记录后驱节点。
+ 2 刚才我们直接把head.next设置为 null，因为整个链表反转后原来的head变成了整个链表的最后一个节点。但现在head节点在递归反转之后不一定是最后一个节点了，所以要记录后驱successor（第 n + 1 个节点），反转之后将head连接上。
+ */
+SingleNode_1 *successor = NULL; // 后驱节点
+SingleNode_1 *reverseN(SingleNode_1*head, int n) {
+    if (n == 1) {
+        // 记录第 n + 1 个节点
+        successor = head->next;
+        return head;
+    }
+    // 以 head.next 为起点，需要反转前 n - 1 个节点
+    SingleNode_1 *last = reverseN(head->next, n - 1);
+
+    head->next->next = head;
+    // 让反转之后的 head 节点和后面的节点连起来
+    head->next = successor;
+    
+    return last;
+}
+/**
+ 给一个索引区间[m,n]（索引从 1 开始），仅仅反转区间中的链表元素.
+ 首先，如果m == 1，就相当于反转链表开头的n个元素嘛，也就是我们刚才实现的功能reverseN()
+ 如果m != 1怎么办？如果我们把head的索引视为 1，那么我们是想从第m个元素开始反转对吧；如果把head.next的索引视为 1 呢？那么相对于head.next，反转的区间应该是从第m - 1个元素开始的；那么对于head.next.next呢……
+ 
+ 递归操作链表并不高效。和迭代解法相比，虽然时间复杂度都是 O(N)，但是迭代解法的空间复杂度是 O(1)，而递归解法需要堆栈，空间复杂度是 O(N)。
+ 
+ */
+SingleNode_1* reverseBetween(SingleNode_1* head, int m, int n) {
+    // base case
+    if (m == 1) {
+        return reverseN(head, n);
+    }
+    // 前进到反转的起点触发 base case
+    head->next = reverseBetween(head->next, m - 1, n - 1);
+    return head;
+}
