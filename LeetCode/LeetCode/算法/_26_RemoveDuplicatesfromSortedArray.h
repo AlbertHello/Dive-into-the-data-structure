733cd05eba3c3eba7b2f71af84250ed953fa177a
wangqizheng <wangqizheng@58.com>
2021-01-02 11:27:32 +0800

    26. 删除排序数组中的重复项 双指针技巧

---
+++ LeetCode/LeetCode/算法/_26_RemoveDuplicatesfromSortedArray.h
@@ -0,0 +1,16 @@
+//
+//  _26_RemoveDuplicatesfromSortedArray.h
+//  LeetCode
+//
+//  Created by Albert on 2021/1/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _26_RemoveDuplicatesfromSortedArray : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeetCode/LeetCode/算法/_26_RemoveDuplicatesfromSortedArray.m
@@ -0,0 +1,76 @@
+//
+//  _26_RemoveDuplicatesfromSortedArray.m
+//  LeetCode
+//
+//  Created by Albert on 2021/1/2.
+//
+
+#import "_26_RemoveDuplicatesfromSortedArray.h"
+
+@implementation _26_RemoveDuplicatesfromSortedArray
+
+/**
+ 26. 删除排序数组中的重复项
+ 难度 简单
+ https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+ 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+  
+
+ 示例 1:
+
+ 给定数组 nums = [1,1,2],
+
+ 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+ 示例 2:
+
+ 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+ 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+  
+
+ 说明:
+
+ 为什么返回数值是整数，但输出的答案是数组呢?
+
+ 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+ 你可以想象内部操作如下:
+
+ // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ int len = removeDuplicates(nums);
+
+ // 在函数里修改输入数组对于调用者是可见的。
+ // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ for (int i = 0; i < len; i++) {
+     print(nums[i]);
+ }
+ */
+
+/**
+ 双指针
+ 我们让慢指针 slow 走在后面，快指针 fast 走在前面探路，找到一个不重复的元素就告诉 slow 并让 slow 前进一步。这样当 fast 指针遍历完整个数组 nums 后，nums[0..slow] 就是不重复元素。
+ 
+ */
+int removeDuplicates(int* nums, int numsSize){
+    if (numsSize == 0) {
+        return 0;
+    }
+    int slow=0, fast=0;
+    while (fast < numsSize) {
+        if (nums[fast] != nums[slow]) {
+            slow++;
+            nums[slow] = nums[fast];
+        }
+        fast++;
+    }
+    // 数组长度为索引 + 1
+    return slow + 1;
+}
+@end
