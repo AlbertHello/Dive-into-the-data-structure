bb8a1641c4cac03bf2648c616a5fffac77d54655
wangqizheng <wangqizheng@58.com>
2021-01-02 17:51:13 +0800

    98. 验证二叉搜索树

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -1447,6 +1447,49 @@ static NSMutableArray *result_113 = nil;
     int maxv = [bt maxPathSum:root];
     NSLog(@"maxPathSum: %d",maxv);
 }
+#pragma mark - 98. 验证二叉搜索树
+//************************* 98. 验证二叉搜索树 *************************
+/**
+ 98. 验证二叉搜索树
+ 难度 中等
+ https://leetcode-cn.com/problems/validate-binary-search-tree/
+ 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ 假设一个二叉搜索树具有如下特征：
+ 节点的左子树只包含小于当前节点的数。
+ 节点的右子树只包含大于当前节点的数。
+ 所有左子树和右子树自身必须也是二叉搜索树。
+ 示例 1:
+ 输入:
+     2
+    / \
+   1   3
+ 输出: true
+ 示例 2:
+ 输入:
+     5
+    / \
+   1   4
+      / \
+     3   6
+ 输出: false
+ 解释: 输入为: [5,1,4,null,null,3,6]。
+      根节点的值为 5 ，但是其右子节点值为 4 。
+ */
+
+bool isValidBST(BTNode *root) {
+    return verifyBST(root, NULL, NULL);
+}
+/* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+bool verifyBST(BTNode *root, BTNode * min, BTNode * max) {
+    // base case
+    if (root == NULL) return true;
+    // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+    if (min != NULL && root.data <= min.data) return false;
+    if (max != NULL && root.data >= max.data) return false;
+    // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+    return verifyBST(root.left, min, root) && verifyBST(root.right, root, max);
+}
+
 
 
 @end
