6ce5959a6b05b0d9cb148224607e8536836a1fab
wangqizheng <wangqizheng@58.com>
2021-01-02 18:28:58 +0800

    在 BST 中搜索一个数

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -1540,6 +1540,38 @@ BTNode * insertIntoBST(BTNode * root, int val) {
     return root;
 }
 
-
+#pragma mark - 在 BST 中搜索一个数
+//************************* 在 BST 中搜索一个数 *************************
+BTNode *deleteNodeInBST(BTNode * root, int key) {
+    if (root == NULL) return NULL;
+    if (root.data == key) {
+        // 找到了。删除节点的同时不能破坏 BST 的性质。有三种情况
+        // 1 恰好是末端节点，两个子节点都为空，那么它可以当场去世了。
+        // 2 只有一个非空子节点，那么它要让这个孩子接替自己的位置
+        // 3 有两个子节点，麻烦了，为了不破坏 BST 的性质，A 必须找到左子树中最大的那个节点，或者右子树中最小的那个节点来接替自己。我们以第二种方式讲解。
+        
+        // 这两个 if 把情况 1 和 2 都正确处理了
+        if (root.left == NULL) return root.right;
+        if (root.right == NULL) return root.left;
+        
+        // 处理情况 3
+        BTNode *minNode = getRightMin(root.right);
+        root.data = minNode.data; //把root.data 直接替换成minNode的值
+        // 下一步就是删除那个minNode
+        root.right = deleteNodeInBST(root.right, minNode.data);
+    } else if (root.data > key) {
+        // 去左子树中
+        root.left = deleteNodeInBST(root.left, key);
+    } else if (root.data < key) {
+        // 去右子树中
+        root.right = deleteNodeInBST(root.right, key);
+    }
+    return root;
+}
+BTNode *getRightMin(BTNode * node) {
+    // BST 最左边的就是最小的
+    while (node.left != NULL) node = node.left;
+    return node;
+}
 
 @end
