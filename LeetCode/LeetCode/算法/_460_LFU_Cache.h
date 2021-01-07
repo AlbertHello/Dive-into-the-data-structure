8c585d9a8dbd9ae20fe12d832a1df01fcd6060a2
wangqizheng <wangqizheng@58.com>
2021-01-07 21:20:29 +0800

    添加460. LFU 缓存 $

---
+++ LeetCode/LeetCode/算法/_460_LFU_Cache.h
@@ -0,0 +1,16 @@
+//
+//  _460_LFU_Cache.h
+//  LeetCode
+//
+//  Created by 58 on 2021/1/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _460_LFU_Cache : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
