487644b9efbda2e2a95e6f379c15a6703dd1d611
wangqizheng <wangqizheng@58.com>
2021-01-08 16:51:44 +0800

    LFU. 还没写完

--- LeetCode/LeetCode/算法/_460_LFU_Cache.m
+++ LeetCode/LeetCode/算法/_460_LFU_Cache.m
@@ -7,6 +7,63 @@
 
 #import "_460_LFU_Cache.h"
 
+
+
+
+@interface LFU_Node : NSObject
+@property(nonatomic,copy  )NSString *key;
+@property(nonatomic,strong)LFU_Node *prev;
+@property(nonatomic,strong)LFU_Node *next;
+@end
+
+@implementation LFU_Node
+-(instancetype)init{
+    if (self=[super init]) {
+        self.key=@"";
+        self.prev=nil;
+        self.next=nil;
+    }
+    return self;
+}
+@end
+
+
+
+@interface DLink : NSObject
+@property(nonatomic,strong)LFU_Node *first;
+@property(nonatomic,strong)LFU_Node *last;
+@property(nonatomic,assign)NSUInteger size;
+// 在链表头部添加节点 x，时间 O(1)，头节点代表最新的节点
+-(void)addFront:(LFU_Node *)node;
+// 删除链表中最后一个节点，最后一个节点代表最旧的节点
+// 删除链表中最后一个节点，并返回该节点，时间 O(1)
+-(LFU_Node *)removeLast;
+
+@end
+
+@implementation DLink
+-(void)addFront:(LFU_Node *)node{
+    
+}
+-(LFU_Node *)removeLast{
+    return nil;
+}
+
+
+
+@end
+
+
+@interface _460_LFU_Cache ()
+
+@property(nonatomic,strong)NSMutableDictionary<NSString *, NSObject*> *key_value_map;
+@property(nonatomic,strong)NSMutableDictionary<NSString *, NSObject*>*key_freq_map;
+@property(nonatomic,strong)NSMutableDictionary<NSString *, DLink*> *freq_keys_map;
+@property(nonatomic,assign)NSUInteger capacity;
+@property(nonatomic,assign)NSUInteger minFreq;
+
+@end
+
 @implementation _460_LFU_Cache
 
 
@@ -50,5 +107,53 @@
  0 <= capacity, key, value <= 10^4
  最多调用 10^5 次 get 和 put 方法
  */
+
+-(int)get:(int)key{
+    NSString *key_s=@(key).stringValue;
+    if (!self.key_value_map[key_s]) {
+        return -1;
+    }
+    // 增加 key 对应的 freq
+    [self increaseFreq:key];
+    NSNumber *value=(NSNumber *)self.key_value_map[key_s];
+    return value.intValue;
+}
+-(void)put:(int)key value:(int)val{
+    if (self.capacity <= 0) return;
+    /* 若 key 已存在，修改对应的 val 即可 */
+    NSString *key_s=@(key).stringValue;
+    if (self.key_value_map[key_s]) {
+        self.key_value_map[key_s]=@(val);
+        // key 对应的 freq 加一
+        [self increaseFreq:key];
+        return;
+    }
+
+    /* key 不存在，需要插入 */
+    /* 容量已满的话需要淘汰一个 freq 最小的 key */
+    if (self.capacity == self.key_value_map.count) {
+        [self removeMinFreqKey:key];
+    }
+
+    /* 插入 key 和 val，对应的 freq 为 1 */
+    // 插入 KV 表
+    [self.key_value_map setObject:@(val) forKey:key_s];
+    
+    // 插入 KF 表
+    [self.key_freq_map setObject:@(1) forKey:key_s];
+    
+    // 插入 FK 表
+//    freqToKeys.putIfAbsent(1, new LinkedHashSet<>());
+//
+//    freqToKeys.get(1).add(key);
+    // 插入新 key 后最小的 freq 肯定是 1
+    self.minFreq = 1;
+}
+-(void)increaseFreq:(int)key{
+    
+}
+-(void)removeMinFreqKey:(int)key{
+    
+}
 
 @end
