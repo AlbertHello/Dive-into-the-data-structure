dc261444403e7ce10eb04746925ff5e13d504e44
wangqizheng <wangqizheng@58.com>
2021-01-16 19:52:41 +0800

    110. 平衡二叉树 ¥

--- LeetCode/LeetCode/二叉树/BinaryTree.m
+++ LeetCode/LeetCode/二叉树/BinaryTree.m
@@ -1659,4 +1659,44 @@ int countNodes2(BTNode * root) {
     // 算法的递归深度就是树的高度 O(logN)，每次递归所花费的时间就是 while 循环，需要 O(logN)，所以总体的时间复杂度是 O(logN*logN)。
     return 1 + countNodes2(root.left) + countNodes2(root.right);
 }
+
+#pragma mark - 110. 平衡二叉树 ¥
+//************************* 110. 平衡二叉树 ¥ *************************
+/**
+ 110. 平衡二叉树 ¥
+ 难度 简单
+ 链接：https://leetcode-cn.com/problems/balanced-binary-tree/
+ 给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+ 本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1
+ 
+ 自底向上递归的做法类似于后序遍历，对于当前遍历到的节点，先递归地判断其左右子树是否平衡，再判断以当前节点为根的子树是否平衡。如果一棵子树是平衡的，则返回其高度（高度一定是非负整数），否则返回−1。如果存在一棵子树不平衡，则整个二叉树一定不平衡。
+
+ 时间复杂度：O(n)，其中n 是二叉树中的节点个数。使用自底向上的递归，每个节点的计算高度和判断是否平衡都只需要处理一次，
+ 最坏情况下需要遍历二叉树中的所有节点，因此时间复杂度是O(n)。
+ 空间复杂度：O(n)，其中n 是二叉树中的节点个数。空间复杂度主要取决于递归调用的层数，递归调用的层数不会超过
+ n。
+ */
+int height(BTNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int leftHeight = height(root.left);
+    int rightHeight = height(root.right);
+    if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+        return -1;
+    } else {
+        return max(leftHeight, rightHeight) + 1;
+    }
+}
+bool isBalanced(BTNode* root) {
+    return height(root) >= 0;
+}
+
+
+
+
+
+
+
 @end
